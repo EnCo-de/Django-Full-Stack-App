1cc5ce89f479462a0e3ef763e150f68d88592fa0
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-05-25 13:22:06 +0400

    deploy backend api

--- backend/b/settings.py
+++ backend/b/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'b.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PWD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
