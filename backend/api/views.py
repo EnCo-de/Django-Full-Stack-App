9fc0360ea86d875f5ce8fadda2177807dc910815
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-05-25 11:33:37 +0400

    complete backend/api

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,17 +2,35 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from .serializers import UserSerializer
+from .serializers import NoteSerializer, UserSerializer
+from .models import Note
+
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-
-
-def submit(request):
-    if request.session.has_key('username'):
-        username = request.session['username']
-        return render(request, 'loggedin.html', {'username': username})
-    return render(request, 'login.html')
