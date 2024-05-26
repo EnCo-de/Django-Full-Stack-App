d4e01d9c0184cd53f7e77678ac072abe69ad0ce6
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-05-26 09:04:26 +0400

    add deployed backend url

--- backend/b/urls.py
+++ backend/b/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda x: JsonResponse({'status':'200: OK API is working'})),
     path('api/user/register/', CreateUserView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
